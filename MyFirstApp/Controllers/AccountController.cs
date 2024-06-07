8aee6fff74b950481ca0778b573b69d8d15f5e7d
Yash Gangan <ganganyash160@gmail.com>
2024-06-08 03:11:05 +0530

    learned about actioneresult class & its child classes

---
+++ MyFirstApp/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string username, string password)
+        {
+            if (username == "admin" && password == "manager")
+                return RedirectToAction("Dasboard", "Admin");
+            else
+                return RedirectToAction("InvalidLogin");
+        }
+        public ActionResult InvalidLogin()
+        {
+            return View();
+        }
+    }
+}
--- MyFirstApp/Controllers/HomeController.cs
+++ MyFirstApp/Controllers/HomeController.cs
@@ -21,5 +21,50 @@ namespace MyFirstApp.Controllers
         {
             return View();
         }
+        public ActionResult GetEmpName(int EmpId)
+        {
+            var employees = new[] {
+                new {EmpId = 1, EmpName = "Ram", Salary = 5000},
+                new {EmpId = 2, EmpName = "Sham", Salary = 4000},
+                new {EmpId = 3, EmpName = "John", Salary = 7000},
+                new {EmpId = 4, EmpName = "Mary", Salary = 3000}
+            };
+            string myempname = null;
+            foreach(var item in employees)
+            {
+                if (item.EmpId == EmpId)
+                    myempname = item.EmpName;
+            }
+
+            return Content(myempname, "text/plain");
+        }
+        public ActionResult GetPaySlip(int EmpId)
+        {
+            string fileName = "~/Assets/Payslip" + EmpId + ".pdf";
+            return File(fileName, "application/pdf");
+        }
+        public ActionResult GetEmpFacebook(int EmpId)
+        {
+            var employees = new[] {
+                new {EmpId = 1, EmpName = "Ram", Salary = 5000},
+                new {EmpId = 2, EmpName = "Sham", Salary = 4000},
+                new {EmpId = 3, EmpName = "John", Salary = 7000},
+                new {EmpId = 4, EmpName = "Mary", Salary = 3000}
+            };
+            string fbUrl = null;
+            foreach (var item in employees)
+            {
+                if (item.EmpId == EmpId)
+                    fbUrl = "http://facebook.com/emp" + EmpId;
+            }
+            if(fbUrl == null)
+            {
+                return Content("Invalid Employee Id");
+            }
+            else
+            {
+                return Redirect(fbUrl);
+            }
+        }
     }
 }
